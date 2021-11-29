95c76b11fd713a66736f443643fff1cc1986b4b5
Thibault Bruggeman <bruggeman.dev@gmail.com>
2021-11-29 18:47:44 +0100

    Add Hero component for homepage

---
+++ components/commons/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="btn bg-button-default hover:bg-button-dark text-buttonText hover:text-primary max-w-md mx-auto m-5"
+    @click="goToProjectsPage"
+  >
+    <slot></slot>
+  </button>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Button',
+  methods: {
+    goToProjectsPage() {
+      this.$router.push({ name: 'projets' });
+    },
+  },
+});
+</script>
+<style lang="postcss" scoped>
+.btn {
+  @apply font-bold py-2 px-4 rounded;
+}
+</style>
---
+++ components/index/HeroText.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-headline text-6xl m-5">Thibault Bruggeman</h1>
+    <p class="text-paragraph">
+      Développeur en formation Full Stack à l'IPI de Lyon
+    </p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'HeroText',
+});
+</script>
