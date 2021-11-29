f560394941b987b5f356261611a10469a0e3244b
Thibault Bruggeman <bruggeman.dev@gmail.com>
2021-11-29 13:59:21 +0100

    Update README

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,3 +6,16 @@
     <Nuxt class="h-screen flex justify-center content-center"/>
   </div>
 </template>
+
+<style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html {
+  background-color: #fef6e4;
+}
+</style>
