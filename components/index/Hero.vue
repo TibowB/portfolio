78af8c83d7376467397bc18989ed6a83caec0ad4
Thibault Bruggeman <bruggeman.dev@gmail.com>
2021-12-06 19:41:43 +0100

    Adding hover and margin

--- components/index/Hero.vue
+++ components/index/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-center content-center text-center">
     <HeroText />
-    <div class="flex flex-row">
+    <div class="flex flex-row mt-5">
       <Button page="projets">Voir mes projets</Button>
       <Button page="contact">Me contacter</Button>
     </div>
