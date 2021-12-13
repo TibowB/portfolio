35a3c24f7d889c4f8b8da7dfa40c08f1aa450b1e
Thibault Bruggeman <bruggeman.dev@gmail.com>
2021-12-13 10:23:04 +0100

    Update home page

--- components/commons/NavBar.vue
+++ components/commons/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex justify-center m-2">
+  <nav class="flex justify-center m-2 w-full">
     <NuxtLink to="/">
       <h1
         class="text-2xl uppercase text-headline font-sans tracking-widest transform transition duration-300 hover:scale-110"
