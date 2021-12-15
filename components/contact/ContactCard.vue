5dba7328845ab290cdcc53e8a9e6bd244c07d70a
Thibault Bruggeman <bruggeman.dev@gmail.com>
2021-12-15 20:33:15 +0100

    Add contact view components

---
+++ components/contact/ContactCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mx-5 min-h-screen grid place-content-center">
+    <div
+      class="bg-card py-8 px-10 text-center rounded-md shadow-lg transform -translate-y-20 sm:-translate-y-24 max-w-s mx-auto"
+    >
+      <img
+        class="w-40 h-40 object-cover rounded-full mx-auto shadow-lg"
+        src="/img/contact.jpeg"
+        alt="Thibault Bruggeman contact picture"
+      />
+      <p class="capitalize text-2xl text-buttonText m-3">Thibault Bruggeman</p>
+      <div class="flex flex-row space-x-4 justify-center mt-2 items-center">
+        <ContactIcon url="https://github.com/TibowB" icon="github" />
+        <ContactIcon
+          url="https://www.linkedin.com/in/thibault-bruggeman-435b0687/"
+          icon="linkedin"
+        />
+        <ContactIcon url="mailto:bruggeman.dev@gmail.com" icon="email" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'ContactCard',
+});
+</script>
