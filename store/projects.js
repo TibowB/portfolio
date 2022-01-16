62c6ffd7fdd6b95f5ab36f3b6f38fec298b933ba
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-01-16 19:46:42 +0100

    Start project component

---
+++ store/projects.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+  projects: [
+    {
+      id: 1,
+      name: 'Random Color Generator',
+      imagePath: '/img/projects/random.png',
+      imageAlt: 'Image of the project Random Color Generator',
+      live: 'https://random-color-generator.bruggeman.dev',
+      code: 'https://github.com/TibowB/random-color-generator',
+    },
+    {
+      id: 2,
+      name: 'CityHall',
+      imagePath: '/img/projects/cityhall.png',
+      imageAlt: 'Image of the project CityHall',
+      live: 'https://cityhall.bruggeman.dev',
+      code: 'https://github.com/TibowB/cityhall',
+    },
+  ],
+});
