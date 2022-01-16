406a2d574f8aadedeeadb4572ee730ba3aace0bb
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-01-16 19:46:42 +0100

    Start project component

---
+++ components/project/ProjectCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mx-5 min-h-screen grid place-content-center">
+    <div
+      class="bg-card py-4 px-6 text-center rounded-md shadow-lg transform -translate-y-20 sm:-translate-y-24 max-w-s mx-auto"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'ProjectCard',
+});
+</script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,18 @@
 <template>
-  <ProjectCard />
+  <div class="flex justify-center content-center text-center mx-5">
+    <ProjectCard v-for="project in projects" :key="project.id">
+      <h4 class="font-semibold mb-2">{{ project.name }}</h4>
+      <img
+        class="max-w-80 min-w-60 max-h-70 min-h-60 rounded-md mx-auto shadow-lg"
+        :src="project.imagePath"
+        :alt="project.imageAlt"
+      />
+      <div class="flex flex-row justify-between items-center">
+        <Button :page="project.live" isProject> Live </Button>
+        <Button :page="project.code" isProject> Code </Button>
+      </div>
+    </ProjectCard>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,6 +21,11 @@ import Vue from 'vue';
 export default Vue.extend({
   name: 'Projects',
   layout: 'default',
+  computed: {
+    projects() {
+      return this.$store.state.projects.projects;
+    },
+  },
   head() {
     return {
       title: 'Projects',
